370022bbf0dd6fab7d7802407908416190fc2dc7
romz <romz@wp.pl>
2024-12-19 16:33:46 +0100

    Move AMPL model to separate file

--- app.py
+++ app.py
@@ -2,86 +2,62 @@ import streamlit as st
 from amplpy import AMPL, modules
 import os
 
-uuid = os.environ.get("AMPLKEY_UUID")  # Use a free https://ampl.com/ce license
-if uuid is not None:
-    modules.activate(uuid)  # activate your license
+
+def run_ampl(nqueens):
+    ampl = AMPL()
+    ampl.read("./ampl_model.txt")
+    ampl.option["solver"] = "highs"
+    ampl.option["highs_options"] = "outlev=2"
+    ampl.param["n"] = nqueens
+    output = ampl.get_output("solve;")
+    solution = ampl.get_data("Row").to_dict()
+    return output, solution
 
 
-"""
-# N-Queens
-
-**How can $n$ queens be placed on an $n \\times n$ chessboard so that no two of them attack each other?**
-
-Constraint `alldiff` enforces a set of integer variables to take distinct values. Using `alldiff`, we can model N-Queens as follows:
-
-```ampl
-param n integer > 0; # N-queens
-var Row {1..n} integer >= 1 <= n;
-s.t. row_attacks: alldiff ({j in 1..n} Row[j]);
-s.t. diag_attacks: alldiff ({j in 1..n} Row[j]+j);
-s.t. rdiag_attacks: alldiff ({j in 1..n} Row[j]-j);
-```
-"""
-
-ampl = AMPL()
-ampl.eval(
-    """
-param n integer > 0; # N-queens
-var Row {1..n} integer >= 1 <= n;
-s.t. row_attacks: alldiff ({j in 1..n} Row[j]);
-s.t. diag_attacks: alldiff ({j in 1..n} Row[j]+j);
-s.t. rdiag_attacks: alldiff ({j in 1..n} Row[j]-j);
-"""
-)
-ampl.option["solver"] = "highs"
-ampl.option["highs_options"] = "outlev=1"
-
-n = st.slider("How many queens?", 2, 25, 8)
-
-ampl.param["n"] = n
-output = ampl.get_output("solve;")
-solution = ampl.get_data("Row").to_dict()
-queens = set((int(r) - 1, int(c) - 1) for c, r in solution.items())
-
-st.write("### Solution")
-solution = "#" + " # " * (n) + "#\n"
-for r in range(n):
-    row = "".join([" Q " if (r, c) in queens else " + " for c in range(n)])
-    # st.write(f"`{row}`")
-    solution += "#" + row + "#\n"
-solution += "#" + " # " * (n) + "#\n"
-st.write(f"```\n{solution}\n```")
+def show_problem_definition():
+    st.subheader("Problem definition", divider="blue")
+    st.markdown(":red[How can $N$ queens be placed on an $N \\times N$ chessboard so that no two of them attack each other?]")
 
 
-st.write("# HiGHS output")
-st.write(f"```\n{output}\n```")
+def get_ampl_license():
+    uuid = os.environ.get("AMPLKEY_UUID")  # Use a free https://ampl.com/ce license
+    if uuid is not None:
+        modules.activate(uuid)  # activate your license
 
-"""
 
-## AMPL :heart: Python :heart: Streamlit
+def show_solution(n, solution):
+    st.subheader("Solution", divider="blue")
+    queens = set((int(r) - 1, int(c) - 1) for c, r in solution.items())
+    solution = "#" + " # " * (n) + "#\n"
+    for r in range(n):
+        row = "".join([" Q " if (r, c) in queens else " + " for c in range(n)])
+        solution += "#" + row + "#\n"
+    solution += "#" + " # " * (n) + "#\n"
+    st.code(solution, language=None)
 
-**Deploy optimization apps to [Streamlit Cloud](https://streamlit.io/) with AMPL**
 
-[AMPL and all Solvers are now available as Python Packages.](https://dev.ampl.com/ampl/python/)
-To use them in Streamlit you just need to list the modules in the [requirements.txt](https://github.com/fdabrandao/streamlit-nqueens/blob/master/requirements.txt) file as follows:
-```
---index-url https://pypi.ampl.com # AMPL's Python Package Index
---extra-index-url https://pypi.org/simple
-ampl_module_base # AMPL
-ampl_module_highs # HiGHS solver
-ampl_module_gurobi # Gurobi solver
-amplpy # Python API for AMPL
-```
+def provide_input_parameters():
+    st.subheader("Provide input parameters", divider="blue")
+    nqueens = st.slider("How many queens?", 2, 25, 8)
+    return nqueens
 
-and then just load them in [streamlit_app.py](https://github.com/fdabrandao/streamlit-nqueens/blob/master/streamlit_app.py):
-```python
-from amplpy import AMPL
-ampl = AMPL()
-```
 
-- GitHub repository for this app: https://github.com/fdabrandao/streamlit-nqueens
-- Python API documentation: https://amplpy.readthedocs.io
-- Python modules documentation: https://dev.ampl.com/ampl/python/
+def show_highs_output(output):
+    st.subheader("HiGHS output", divider="blue")
+    st.code(output)
 
-[![Hits](https://h.ampl.com/https://nqueens-with-ampl.streamlit.app/)](https://github.com/fdabrandao/streamlit-nqueens)
-"""
+
+def main():
+    st.title("N-Queens")
+    get_ampl_license()
+    show_problem_definition()
+    n = provide_input_parameters()
+    output, solution = run_ampl(n)
+    show_solution(n, solution)
+    show_highs_output(output)
+
+
+
+
+if __name__ == "__main__":
+    main()
